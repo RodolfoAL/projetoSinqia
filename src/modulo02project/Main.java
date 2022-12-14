aad363a7742106c00936bb1ab6893083f4af7ded
Rodolfo Lisboa <rodolfoassislisboa@gmail.com>
2022-12-14 18:43:24 -0300

    Atualização 02

---
+++ src/modulo02project/Main.java
@@ -0,0 +1,8 @@
+package modulo02project;
+
+public class Main {
+    public static void main(String[] args) {
+        RedeSocial redeSocial = new RedeSocial();
+        redeSocial.start();
+    }
+}
