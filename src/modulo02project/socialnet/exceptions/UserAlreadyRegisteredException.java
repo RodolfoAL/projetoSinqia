598e7c9f013629c52d35dfc0c8a2693f6f7feb20
Rodolfo Lisboa <rodolfoassislisboa@gmail.com>
2022-12-19 20:02:27 -0300

    Atualização 07

--- src/modulo02project/socialnet/exceptions/UserAlreadyRegisteredException.java
+++ src/modulo02project/socialnet/exceptions/UserAlreadyRegisteredException.java
@@ -2,6 +2,6 @@ package modulo02project.socialnet.exceptions;
 
 public class UserAlreadyRegisteredException extends RuntimeException {
     public String getMessage() {
-        return "J√° existe um usu√°rio cadastrado com este login!";
+        return "J· existe um usu·rio cadastrado com este login!";
     }
 }
--- src/modulo02project/socialnet/exceptions/UserNotFoundException.java
+++ src/modulo02project/socialnet/exceptions/UserNotFoundException.java
@@ -2,6 +2,6 @@ package modulo02project.socialnet.exceptions;
 
 public class UserNotFoundException extends RuntimeException {
     public String getMessage() {
-        return "Este login n√£o foi encontrado. Por favor, entre com um login j√° cadastrado para entrar.";
+        return "Este login n„o foi encontrado. Por favor, entre com um login j· cadastrado para entrar.";
     }
 }
